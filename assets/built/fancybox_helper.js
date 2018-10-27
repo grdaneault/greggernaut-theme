1405f37e1accc45ec73a9a462638fe0246a81fb4
Greg Daneault <gregdaneault@gmail.com>
2018-10-27 18:48:59 -0400

    Theme Update from Master
    Fix vertical spacing after update
    Rebuild assets
    Add missing assets

---
+++ assets/built/fancybox_helper.js
@@ -0,0 +1,2 @@
+!function(t){t(".kg-gallery-image img").each(function(e,r){var a,p;console.log(r),a=r,p="gallery",wrapper=document.createElement("a"),t(wrapper).attr("href",a.src),t(wrapper).attr("data-fancybox",p),t(wrapper).insertBefore(t(a)),t(wrapper).append(t(a))})}($);
+//# sourceMappingURL=fancybox_helper.js.map
